8e2efcf7624448d27ee6424fb6c195682d12e14d
Agustin <darkstone19@gmail.com>
2023-04-19 21:58:19 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/*
+ EJERCICIO 19 : Defina una función que invierte una cadena de caracteres dada, y la devuelve.
+ */
+
 int main() {
     printf("Hello, World!\n");
     return 0;
